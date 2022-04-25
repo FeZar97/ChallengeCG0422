a2ae0632018ca9dd973196e029015d0ce9587246
FeZar97 <bhd2010@yandex.ru>
2022-04-26 01:06:13 +0300

    added heuristic for most dangerous monster in the vicinity of base; added using WIND spell

--- main.cpp
+++ main.cpp
@@ -9,7 +9,13 @@
 #include <vector>
 
 using namespace std;
-using Actions = vector<string>;
+struct Action {
+    std::string action;
+    int val1;
+    int val2{ -1 };
+    int val3{ -1 };
+};
+using Actions = vector<Action>;
 
 struct BaseStats {
     int health, mana;
@@ -33,7 +39,7 @@ double dist(Coord c1, Coord c2) {
     int dx = abs(c1.x - c2.x), dy = abs(c1.y - c2.y);
     return sqrt(dx * dx + dy * dy);
 }
-const int cBaseRadius{ 5000 };
+const double cBaseRadius{ 5000 };
 const Coord maxCoord{ 17630, 9000 };
 const double cMaxDist = dist({ 0, 0 }, maxCoord);
 
@@ -49,13 +55,14 @@ struct Entity {
     double distanceToBase{ 0. };
     int stepsToBase{ 0 };
     int neededUnitsNb{ 0 }; // МИНИМАЛЬНОЕ количество юнитов, требующееся для убийства монстра
+    int reachCordonSteps{ 0 };
+    Coord simulatedCoords;
 
     void read() {
         cin >> id >> type >> coords.x >> coords.y >> shieldLife >> isControlled >> health >> vx >> vy >> nearBase >> threatFor; cin.ignore();
     }
 };
-bool entityCompare(const Entity& left, const Entity& right)
-{
+bool entityCompare(const Entity& left, const Entity& right) {
     return (left.importance > right.importance);
 }
 
@@ -65,7 +72,7 @@ struct Entities {
     int entitiesNb;
     Entity tempEntity;
 
-    vector<Entity> dangerousMonsters;
+    vector<Entity> dangerousMonsters, undangerousMonsters;
 
     vector<vector<Coord>> waitCoordsVec2;
 
@@ -75,7 +82,7 @@ struct Entities {
     }
 
     void read() {
-        monsters.clear(); our.clear(); enemies.clear(); dangerousMonsters.clear();
+        monsters.clear(); our.clear(); enemies.clear(); dangerousMonsters.clear(); undangerousMonsters.clear();
         cin >> entitiesNb; cin.ignore();
         for (int entityId = 0; entityId < entitiesNb; entityId++) {
             tempEntity.read();
@@ -89,35 +96,56 @@ struct Entities {
     }
 
     Actions updateDangerousMonsters() {
-        int maxSimulationDepth = 70;
-        set<int> accountedInDistToBaseIdxs; // учтенные в расчете дистанции до базы (попадают в этот сет, когда расстояние до базы <= 400)
+        int maxSimulationDepth = 35;
+
+        for (Entity& monster : monsters) {
+            monster.simulatedCoords = monster.coords;
+        }
+        vector<Entity> safetyOur;
+        for (const Entity& ourUnit : our) {
+            safetyOur.push_back(ourUnit);
+        }
+
+        set<int> accountedDangeroudIdxs; // учтенные в расчете дистанции до базы (попадают в этот сет, когда расстояние до базы <= 400)
         for (int simulationDepth = 0; simulationDepth < maxSimulationDepth; simulationDepth++) {
             // simulate monster pos for simulationDepth iteration
             for (Entity& monster : monsters) {
 
-                Coord simulatedCoord = { monster.coords.x + monster.vx, monster.coords.y + monster.vy };
+                monster.simulatedCoords.x += monster.vx;
+                monster.simulatedCoords.y += monster.vy;
 
-                // if (dist(monster.coords, baseCoords) <= cBaseRadius) {
-                //     simulatedCoord = { monster.coords.x + monster.vx, monster.coords.y + monster.vy };
-                // }
-                // else {
-                //     simulatedCoord = { monster.coords.x + simulationDepth * monster.vx, monster.coords.y + simulationDepth * monster.vy };
-                // }
+                double distToBase = dist(monster.simulatedCoords, baseCoords);
 
-                double distToBase = dist(simulatedCoord, baseCoords);
+                // меняеем вектор скорости, когда монстр заходит в радиус базы
+                if (distToBase < cBaseRadius)
+                {
+                    int dBaseX = baseCoords.x - monster.simulatedCoords.x,
+                        dBaseY = baseCoords.y - monster.simulatedCoords.y;
+                    monster.vx = Entity::cMonsterSpeed * dBaseX / distToBase;
+                    monster.vy = Entity::cMonsterSpeed * dBaseY / distToBase;
+
+                    if (!monster.reachCordonSteps)
+                    {
+                        monster.reachCordonSteps = simulationDepth;
+                        // cerr << "Monster " << monster.id << " will reach our base cordon in " << monster.reachCordonSteps << " steps" << endl;
+                        // cerr << "Monster " << monster.id << " change his V to " << monster.vx << " " << monster.vy << endl;
+                    }
+
+                    // cerr << "On simulation " << simulationDepth << " monster " << monster.id << " speed: " << monster.vx << " " << monster.vy << endl;
+                }
 
                 // добавляем дистанцию к общей дистанции до базы за тот ход, который симулируем
                 // если монстр у базы, то для него в последующих симуляциях не надо считать дистанцию
-                if (!accountedInDistToBaseIdxs.count(monster.id)) {
+                if (!accountedDangeroudIdxs.count(monster.id)) {
 
                     monster.distanceToBase += Entity::cMonsterSpeed;
                     monster.stepsToBase++;
 
-                    if (distToBase < 700) {
+                    if (distToBase < 500) {
                         monster.importance = (maxSimulationDepth - simulationDepth - 1) * 2500.;
-                        monster.neededUnitsNb = static_cast<int>(std::ceil(static_cast<double>(monster.health) / (monster.stepsToBase * Entity::cUnitDamage)));
+                        monster.neededUnitsNb = static_cast<int>(std::ceil(static_cast<double>(monster.health) / (monster.stepsToBase * Entity::cUnitDamage))) + 1;
 
-                        accountedInDistToBaseIdxs.insert(monster.id);
+                        accountedDangeroudIdxs.insert(monster.id);
                         dangerousMonsters.push_back(monster);
                     }
 
@@ -125,21 +153,31 @@ struct Entities {
             }
         }
 
-        cerr << "Sorting dangerous monsters, size: " << dangerousMonsters.size() << endl;
+        // сохраняем монстров, которые не представляют угрозы, чтобы пофармить их если буду свободные юниты
+        for (Entity& monster : monsters) {
+            if (!accountedDangeroudIdxs.count(monster.id)) {
+                monster.importance = (cMaxDist - dist(monster.coords, baseCoords)) * 2500. / cMaxDist;
+                undangerousMonsters.push_back(monster);
+            }
+        }
+
+        // cerr << "Sorting dangerous monsters, size: " << dangerousMonsters.size() << endl;
+        // cerr << "Sorting undangerousMonsters monsters, size: " << dangerousMonsters.size() << endl;
 
         // сортировка по важности
         sort(dangerousMonsters.begin(), dangerousMonsters.end(), entityCompare);
+        sort(undangerousMonsters.begin(), undangerousMonsters.end(), entityCompare);
 
-        cerr << "Creating actions" << endl;
+        // cerr << "Creating actions" << endl;
 
         // формируем ходы для юнитов на основании важности монстра и количества юнитов, которое необходимо для его убийства
         Actions actions;
         actions.resize(3);
         for (Entity& monster : dangerousMonsters) {
 
-            cerr << "Monster with id" << monster.id << " has importance " << monster.importance
-                << ", dist to base " << monster.distanceToBase << ", stepsToBase " << monster.stepsToBase
-                << " and need units: " << monster.neededUnitsNb << endl;
+            // cerr << "Monster with id " << monster.id << " has importance " << monster.importance
+            //     << ", dist to base " << monster.distanceToBase << ", stepsToBase " << monster.stepsToBase
+            //     << " and need units: " << monster.neededUnitsNb << endl;
 
             for (int neededUnitIdx = 0; neededUnitIdx < monster.neededUnitsNb; neededUnitIdx++) {
                 // ищем ближайшего юнита к данному монстру
@@ -153,14 +191,14 @@ struct Entities {
                     }
                 }
 
-                cerr << "\tBest our unit id" << bestOurUnitId << endl;
+                // cerr << "\tBest our unit id" << bestOurUnitId << endl;
 
-                // определяем индекс команды
+                // определяем индекс юнита (команды)
                 int commandIdx = bestOurUnitId - (bestOurUnitId >= 3 ? 3 : 0);
 
                 // добавляем команду на атаку ближайшим юнитом этого монстра
                 // ТУТ МОЖНО ДОБАВИТЬ ВЕТЕРОК, ЕСЛИ МОНСТР НЕ МЕЖДУ БАЗОЙ И ЮНИТОМ
-                actions[commandIdx] = "MOVE " + std::to_string(monster.coords.x) + " " + std::to_string(monster.coords.y);
+                actions[commandIdx] = { "MOVE", monster.coords.x, monster.coords.y };
 
                 // удаляем выбранного юнита из доступных для хода
                 for (int i = 0; i < our.size(); i++) {
@@ -177,13 +215,83 @@ struct Entities {
         }
 
         if (our.size()) {
-            cerr << "Creating wait actions for " << our.size() << " our units" << endl;
+
+            // если есть неопасные монстры - идем фармить
+            if (undangerousMonsters.size()) {
+
+                for (const Entity& undangerousMonster: undangerousMonsters) {
+                    // ищем ближайшего юнита к данному монстру
+                    double bestDist = cMaxDist, curDist;
+                    int bestOurUnitId = -1;
+
+                    for (Entity& ourUnit : our) {
+                        curDist = dist(ourUnit.coords, undangerousMonster.coords);
+                        if (curDist < bestDist) {
+                            bestDist = curDist;
+                            bestOurUnitId = ourUnit.id;
+                        }
+                    }
+
+                    // определяем индекс юнита (команды)
+                    int commandIdx = bestOurUnitId - (bestOurUnitId >= 3 ? 3 : 0);
+
+                    // добавляем команду на атаку ближайшим юнитом этого монстра
+                    actions[commandIdx] = { "MOVE", undangerousMonster.coords.x, undangerousMonster.coords.y };
+
+                    // удаляем выбранного юнита из доступных для хода
+                    for (int i = 0; i < our.size(); i++) {
+                        if (our[i].id == bestOurUnitId) {
+                            our.erase(our.begin() + i);
+                            break;
+                        }
+                    }
+
+                    if (our.empty()) break;
+                }
+            }
+
+            // cerr << "Creating wait actions for " << our.size() << " our units" << endl;
             for (int i = 0; i < our.size(); i++) {
-                // определяем индекс команды
                 int commandIdx = our[i].id - (our[i].id >= 3 ? 3 : 0);
-                cerr << "\tCommand for unit " << commandIdx << "(" << our[i].id << ")" << endl;
-                actions[commandIdx] = "MOVE " + std::to_string(waitCoordsVec2[our.size() - 1][i].x) + " " + std::to_string(waitCoordsVec2[our.size() - 1][i].y);
-                cerr << "\tAction: " << actions[commandIdx] << endl;
+                actions[commandIdx] = { "MOVE", waitCoordsVec2[our.size() - 1][i].x, waitCoordsVec2[our.size() - 1][i].y };
+            }
+        }
+
+        // если на расстоянии 2-3 ходов от базы есть монстр, для убийства которого нужно 3 героя
+        // 
+        // если САМЫЙ ОПАСНЫЙ монстр на расстоянии 3 хода от базы
+        // то берем ближайшего героя к базе и принудительно ведем его точке на расстоянии 300 от базы под 45 градусов
+        // как только этот юнит будет ближе всех остальных юнитов и монстров к базе - этот юнит юзает ветерок
+        if (dangerousMonsters.size()) {
+
+            double mostDangerMonsterDistToBase = dist(dangerousMonsters[0].coords, baseCoords);
+            double windActionsThreshold = 5 * 400.;
+            if (mostDangerMonsterDistToBase < windActionsThreshold) {
+                // ищем ближайшего юнита к базе
+                double bestUnitDist = cMaxDist, curDist;
+                int bestOurUnitId = -1;
+
+                for (Entity& ourUnit : safetyOur) {
+                    curDist = dist(ourUnit.coords, baseCoords);
+                    if (curDist < bestUnitDist) {
+                        bestUnitDist = curDist;
+                        bestOurUnitId = ourUnit.id;
+                    }
+                }
+
+                // cerr << "Most dangerous monster: " << dangerousMonsters[0].id << ", best unit to WIND is " << bestOurUnitId << endl;
+
+                // определяем индекс юнита (команды)
+                int commandIdx = bestOurUnitId - (bestOurUnitId >= 3 ? 3 : 0);
+
+                // если юнит уже ближе к базе чем монстр и расстояние до монстра < 1280, то дуем
+                if ((bestUnitDist < mostDangerMonsterDistToBase) && (dist(dangerousMonsters[0].coords, safetyOur[commandIdx].coords) < 1280)) {
+                    actions[commandIdx] = { "SPELL WIND", dangerousMonsters[0].coords.x, dangerousMonsters[0].coords.y };
+                }
+                else {
+                    // если юнит еще далеко, то пытаемся обогнать монстра
+                    actions[commandIdx] = { "MOVE", baseCoords.x, baseCoords.y };
+                }
             }
         }
 
@@ -191,9 +299,9 @@ struct Entities {
     }
 
     vector<vector<Coord>> getWaitCoords() {
-        static const double defendRadiusCoef = 1.1;
+        static const double defendRadiusCoef = 1.35;
 
-        cerr << "Base coords: " << baseCoords.x << " " << baseCoords.y << endl;
+        // cerr << "Base coords: " << baseCoords.x << " " << baseCoords.y << endl;
 
         return {
             vector<Coord> {
@@ -227,8 +335,11 @@ int main() {
         entities.read();
 
         auto startTime = std::chrono::high_resolution_clock::now();
-        for (const string& action : entities.updateDangerousMonsters()) {
-            cout << action << endl;
+        for (const Action& action : entities.updateDangerousMonsters()) {
+            cout << action.action << " " << action.val1;
+            if (action.val2 != -1) cout << " " << action.val2;
+            if (action.val3 != -1) cout << " " << action.val3;
+            cout << endl;
         }
         cerr << "time per turn: " << std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - startTime).count() << " ms\n";
     }
